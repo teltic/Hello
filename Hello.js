232aee52312679101c26f02b6e0abd1cabd89e54
Thomas <thbailey@ad.sofi.com>
2020-04-06 10:53:35 -0600

    good changes

--- Hello.js
+++ Hello.js
@@ -1,21 +1,6 @@
+var button = document.querySelector("button");
+var Lala = document.querySelector("h1");
 
-var input = document.getElementById("myInput");
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById("myBtn").click();
-  }
-});
-
-document.getElementById("myInput").addEventListener("keyup", myFunction);
-function myFunction() {
-	var elementValue = document.getElementById("myInput").value;
-    document.getElementById("myInput").innerHTML = elementValue;
-
-    // All JS is copied and pasted; text does not change yet.
-
-
-    function myFunction(input){
-      var elementValue = input.value;
-      document.getElementById("myAnchor").innerHTML = elementValue;
-    }
+button.addEventListener("click", function(){
+  Lala.innerHTML = name1;
+})
